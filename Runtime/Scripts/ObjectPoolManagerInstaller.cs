c349a53c3ab7971dac8a93c14d103d09eadfbd09
supremepanda <furkanbaldir13@gmail.com>
2024-03-13 01:26:55 +0300

    Add object pool manager.

---
+++ Runtime/Scripts/ObjectPoolManagerInstaller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace ObjectPoolManager
+{
+	public class ObjectPoolManagerInstaller : MonoInstaller<ObjectPoolManagerInstaller>
+	{
+		[SerializeField] private ObjectPool _prefab;
+		[SerializeField] private bool _resetOnSceneChange = true;
+		private ObjectPoolManager _objectPoolManager;
+
+		private void Awake()
+		{
+			if (!_resetOnSceneChange)
+				return;
+			SceneManager.activeSceneChanged += SceneManager_ActiveSceneChanged;
+		}
+
+		private void SceneManager_ActiveSceneChanged(Scene arg0, Scene arg1)
+		{
+			_objectPoolManager.ResetPoolManager();
+		}
+
+		private void OnDestroy()
+		{
+			if (!_resetOnSceneChange)
+				return;
+			SceneManager.activeSceneChanged -= SceneManager_ActiveSceneChanged;
+		}
+
+		public override void InstallBindings()
+		{
+			_objectPoolManager = new DefaultObjectPoolManager(_prefab);
+			Container.Bind<ObjectPoolManager>().FromInstance(_objectPoolManager).AsSingle()
+				.NonLazy();
+		}
+	}
+}
---
+++ Samples/Scripts/BallSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace ObjectPoolManager.Samples.Scripts
+{
+	public class BallSpawner : MonoBehaviour
+	{
+		[Inject] private ObjectPoolManager _objectPoolManager;
+		[SerializeField] private Ball _ball;
+		private Queue<Ball> _spawnedBalls = new Queue<Ball>();
+
+		private void Start()
+		{
+			StartCoroutine(SpawnBalls());
+		}
+
+		private IEnumerator SpawnBalls()
+		{
+			while (true)
+			{
+				_objectPoolManager.Spawn(_ball, Vector3.zero, Quaternion.identity);
+				yield return new WaitForSeconds(1f);
+			}
+		}
+	}
+}
